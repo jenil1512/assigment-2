218f6a0cdd4d4149088bc2e576c5ee5309126b1d
Jenil Patel <jenilpatel2004@gmail.com>
2025-05-08 13:46:12 +0930

    add land tile

--- main.py
+++ main.py
@@ -25,4 +25,18 @@ class Tile(ABC):
     def __repr__(self):
         return f"Tile(x={self.__x}, y = {self.__y}, object = {repr(self.__game_object)})"
         
-class WaterTile
+class WaterTile(Tile):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        
+    def display(self):
+        obj = self.get_game_object()
+        if obj:
+            if type(obj).__name__ == "PirateShip":
+                return "U" #player's ship
+            elif type(obj).__name == "EnemyShip":
+                return "u" # Enemy ship
+        return " "
+    
+class LandTile(Tile):
+    
